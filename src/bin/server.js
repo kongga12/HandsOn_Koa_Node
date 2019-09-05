dec501bd9f890e163b9d8ead2a89b32aed3a73f1
Cheol Ho Choo <choo.cheolho@gmail.com>
2019-09-05 13:50:53 +0900

    Added Logger,DB

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,8 +1,26 @@
-require("../lib/bootstrap")
-const config = require("config")
+require('../lib/bootstrap')
+const config = require('config')
+const logger = require('../lib/logger')
+const db = require('../lib/db')
 
 async function main() {
-    console.log("Hello World")
+    const host = config.get('server.host')
+    const port = config.get('server.port')
+
+    try {
+        await db.select(db.raw('1'))
+        logger.debug('Database connected')
+
+        logger.debug(`Server is listening on: ${host}:${port}`)
+    } catch (err) {
+        process.exitCode = 1
+        logger.fatal(err)
+    } finally {
+        logger.debug('finally')
+        logger.debug('Close database')
+        await db.destory()
+        logger.debug('Database closed')
+    }
 }
 
 main()
