6515ea7fec03fd74bf57261e4a4558b1c4815c93
Cheol Ho Choo <choo.cheolho@gmail.com>
2019-10-21 22:47:32 +0900

    WIP

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+const users = require("./users-controller")
+const profiles = require("./profiles-controller")
+const articles = require("./articles-controller")
+const tags = require("./tags-controller")
+
+module.exports = {
+  users,
+  profiles,
+  articles,
+  tags,
+}
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,35 +1,39 @@
-const os = require('os')
-const config = require('config')
-const pino = require('pino')
-const colada = require('pino-colada')
-const miss = require('mississippi')
+const config = require("config")
+const pino = require("pino")
+const colada = require("pino-colada")
+const os = require("os")
+const miss = require("mississippi")
 
 const serializers = {
-    req: req => {
-        return pino.stdSerializers.req(req)
-    },
-    res: pino.stdSerializers.res,
-    err: pino.stdSerializers.err,
-    error: pino.stdSerializers.err,
-    user: user => ({
-        id: user._id,
-    }),
+  req: req => {
+    return pino.stdSerializers.req(req)
+  },
+  res: pino.stdSerializers.res,
+  err: pino.stdSerializers.err,
+  error: pino.stdSerializers.err,
+  user: user => ({
+    id: user._id,
+  }),
 }
 
 const opts = {
-    level: config.get('env.logLevel'),
-    serializers,
-    base: {
-        NODE_ENV: process.env.NODE_ENV,
-        environment: config.get('env.environment'),
-        version: config.get('env.version'),
-        name: config.get('env.name'),
-        pid: process.pid,
-        hostname: os.hostname(),
-    },
+  level: config.get("env.logLevel"),
+  serializers,
+  base: {
+    NODE_ENV: process.env.NODE_ENV,
+    environment: config.get("env.enviroinment"),
+    version: config.get("env.version"),
+    name: config.get("env.name"),
+    pid: process.pid,
+    hostname: os.hostname(),
+  },
 }
 
-const stream = process.env.NODE_ENV === 'production' ? pino.destination(1) : miss.pipeline(colada(), pino.destination(1))
+const stream =
+  process.env.NODE_ENV === "production"
+    ? pino.destination(1)
+    : miss.pipeline(colada(), pino.destination(1))
+
 const logger = pino(opts, stream)
 
 module.exports = logger
